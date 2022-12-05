f8f44b4e8e644eb52d282a75697fe36b119784b4
vivian <vivian.sh.fan@dct-cloud.com>
2022-12-05 08:39:40 +0800

    feat: :sparkles: add decision tree & knn code

---
+++ gererate_data.py
@@ -0,0 +1,71 @@
+import random
+import string
+
+import pandas as pd
+
+feature = ['CurrentAge', 'Weight', 'Height', 'Gender', 'MonthOfBirth',
+           'HairColor', 'Food', 'FeedingMethod', 'Temper',
+           'ExerciseFrequency', 'FirstAlphabetOfName', 'Label']
+
+print(len(feature))
+
+# data distribution
+data_count = 5000
+
+# choice
+gender_choice = ['F', 'M']
+hair_color = ['yellow', 'black', 'white', 'black_and_tan']
+food = ['feed', 'can', 'fresh']
+feeding_method_choice = ['stocking', 'indoor', 'indoor_and_yard']
+temper = ['good', 'bad']
+
+# initialize data of lists.
+rows = []
+for index in range(data_count):
+    current_age = random.randint(0, 9)
+    gender = random.randint(0, len(gender_choice) - 1)
+    weight = random.randint(5, 15)
+    height = random.randint(30, 45)
+    m_of_birth = random.randint(1, 12)
+    feeding_method = random.randint(0, len(feeding_method_choice) - 1)
+    exercise_freq = random.randint(1, 7)
+
+    row = [current_age, weight, height,
+           gender, m_of_birth,
+           random.randint(0, len(hair_color)),
+           random.randint(0, len(food)),
+           feeding_method,
+           random.randint(0, len(temper) - 1),
+           exercise_freq,
+           random.randint(0,
+                          len(list(
+                              string.ascii_uppercase)) - 1)]
+    # calculate rules
+    """
+    1. If current age >= 1:
+        - Female: weight :point_right: 6 ~ 9
+        - Male: weight :point_right: 8 ~ 11
+    2. If current age >= 1:
+        - Height: 35 ~ 41
+    3. Month of birth (If is Fibonacci): 1,2,3,5,8
+    4. Feeding Method: `indoor` or `indoor and yard`
+    5. Exercise frequency per week: 3~7
+    """
+    label = 0
+    if current_age >= 1:
+        if gender == 0 and weight >= 6 and weight <= 9:
+            label = 1
+        elif gender == 1 and weight >= 8 and weight <= 11:
+            label = 1
+        elif height >= 35 and height <= 41:
+            label = 1
+    elif m_of_birth in [1, 2, 3, 5, 8]:
+        label = 1
+    elif feeding_method in [1, 2]:
+        label = 1
+    elif exercise_freq >= 3:
+        label = 1
+    row.append(label)
+    rows.append(row)
+df = pd.DataFrame(rows, columns=feature)
+df.to_csv('inputs/data.csv', index=False)
---
+++ knn.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import neighbors
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('inputs/data.csv')
+features_np = df.columns.values
+print(features_np)
+features_np = np.delete(features_np, -1)
+print(features_np)
+
+y = df['Label']                   # 變出 y 資料
+x = df.drop(['Label'], axis=1)
+print(x)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+# algorithm{‘auto’, ‘ball_tree’, ‘kd_tree’, ‘brute’}, default=’auto’
+model = neighbors.KNeighborsClassifier(n_neighbors=5, algorithm='brute')
+model.fit(x_train, y_train)
+
+print(model.score(x_test, y_test))
+print(model.classes_)
+
+y_prediction = model.predict(x_test)
+print(classification_report(y_test, y_prediction))
+
+confmat = confusion_matrix(y_true=y_test, y_pred=y_prediction)
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+plt.xlabel('predicted label')
+plt.ylabel('true label')
+plt.show()
